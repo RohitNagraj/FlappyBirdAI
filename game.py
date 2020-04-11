f21788522aaa65ba5d0d4f1aa2eebb773bfdc8ab
Rohit Nagraj <rohitnagraj.99@gmail.com>
2020-04-11 20:43:43 +0530

    Game completed>

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
+import pickle
 import os
-import time
 
 import neat
 import pygame
@@ -10,6 +10,8 @@ from pipe import Pipe
 
 pygame.font.init()
 
+GEN = 0
+
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
@@ -18,7 +20,8 @@ BG_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bg
 STAT_FONT = pygame.font.SysFont('comicsans', 50)
 
 
-def draw_window(win, bird, pipes, base, score):
+def draw_window(win, birds, pipes, base, score):
+    global GEN
     win.blit(BG_IMAGE, (0, 0))
 
     for pipe in pipes:
@@ -27,13 +30,29 @@ def draw_window(win, bird, pipes, base, score):
     text = STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
 
+    text = STAT_FONT.render("Gen: " + str(GEN), 1, (255, 255, 255))
+    win.blit(text, (10, 10))
+
     base.draw(win)
-    bird.draw(win)
+    for bird in birds:
+        bird.draw(win)
     pygame.display.update()
 
 
-def main():
-    bird = Bird(230, 350)
+def main(genomes, config):
+    global GEN
+    GEN += 1
+    nets = []
+    ge = []
+    birds = []
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+
     base = Base(730)
 
     pipes = [Pipe(600)]
@@ -49,41 +68,83 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+                quit()
 
-        # bird.move()
+        pipe_idx = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_idx = 1
+
+        else:
+            break
+
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_idx].height),
+                                       abs(bird.y - pipes[pipe_idx].bottom)))
+
+            if output[0] > 0.5:
+                bird.jump()
 
         add_pipe = False
         rem = []
         for pipe in pipes:
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
 
-            if pipe.collide(bird):
-                pass
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
 
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                 rem.append(pipe)
-
-            if not pipe.passed and pipe.x < bird.x:
-                pipe.passed = True
-                add_pipe = True
 
             pipe.move()
 
         if add_pipe:
             score += 1
+            for g in ge:
+                g.fitness += 5
             pipes.append(Pipe(600))
 
         for r in rem:
             pipes.remove(r)
 
-        if bird.y + bird.image.get_height() >= 730:
-            pass
+        for x, bird in enumerate(birds):
+            if bird.y + bird.image.get_height() >= 730 or bird.y < 0:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
+
+        if score > 50:
+            break
 
         base.move()
-        draw_window(win, bird, pipes, base, score)
+        draw_window(win, birds, pipes, base, score)
 
-    pygame.quit()
-    quit()
+
+def run_game(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation, config_path)
+
+    population = neat.Population(config)
+
+    population.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    population.add_reporter(stats)
+
+    winner = population.run(main, 50)
+    pickle.dump(winner, open("./BestBird.pkl"))
 
 
 if __name__ == '__main__':
-    main()
+    local_dir = os.path.dirname('__file__')
+    config_file = os.path.join(local_dir, 'neat_config.txt')
+    run_game(config_file)
